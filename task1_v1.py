32b08b26e2e2c97ce3285cf93e1eac0b54385f17
socAriaLaurenson <alaurenson@storans.school.nz>
2025-02-11 11:32:25 +1300

    Add files via upload

---
+++ task1_v1.py
@@ -0,0 +1,24 @@
+#display kia ora!
+
+print("Kia ora!")
+
+#ask whats your name
+
+name = input("what is your name: ")
+
+#saying hello
+
+print("Hello, " +name)
+
+#asking for the users age
+
+age = input("How old are you: ")
+
+#next year
+
+next_year = int(age)+ 1
+#Age next year
+print("Next year you will be?")
+print(next_year)
+
+
